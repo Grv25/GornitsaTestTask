fc9c839f523f464f22dce48cdd34ddb9cdd0353e
Grv25 <151151625+Grv25@users.noreply.github.com>
2023-12-28 17:21:03 +0300

    first commit

---
+++ SPinfo/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace SPinfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(101, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(177, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите артикул стеклопакета:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(177, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(359, 94);
+            this.label2.TabIndex = 3;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(182, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Вывести информацию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(723, 403);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SP-info";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SPinfo/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SPinfo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int spThickness = 0;  // Толщина стеклопакета.
+            int glassThickness = 0;  // Толщина стекла.
+            int slashes = 0;   // Количество символов '/' в артикуле.
+            string[] subs = textBox1.Text.Split('/');  // Части артикула, разделенные символом '/'.
+
+            foreach (char c in textBox1.Text)   // Считаем количество символов '/' в артикуле.
+                if (c == '/') slashes++;
+            
+            for (int i = 0; i < subs.Length; i++)  // Оставляем только числа артикула, другие символы убираем.
+            {
+                int digits = 0;
+
+                subs[i] = subs[i].Trim(); // Удаляем лишние пробелы в начале и конце.
+
+                foreach (char c in subs[i])
+                {
+                    if (char.IsDigit(c))
+                        digits++;
+                    else
+                        break;
+                }
+
+                subs[i] = subs[i].Substring(0, digits);
+
+                if (subs[i] == "")  // Если числа нет, значит некорректный артикул. В этом случае ничего не делаем.
+                {
+                    label2.Text = "Некорректный артикул";
+                    return;
+                }
+            }
+            
+            for (int i = 0; i < subs.Length; i++)  // Считаем толщину всего стеклопакета и толщину стекол.
+            {
+                spThickness += int.Parse(subs[i]);
+
+                if (i % 2 == 0)
+                    glassThickness += int.Parse(subs[i]);
+            }
+
+
+                // Вывод информации:
+
+            if (slashes == 2)   // Информация о количестве камер СП.
+                label2.Text = "Однокамерный";
+            else if (slashes == 4)
+                label2.Text = "2 камерный";
+            else
+            {
+                label2.Text = "Некорректный артикул";
+                return;
+            }
+
+            label2.Text += '\n' + "Толщина всего СП: " + spThickness.ToString();  // Информация о толщине всего стеклопакета и толщине стекол.
+            label2.Text += '\n' + "Толщина стекла в данном СП: " + glassThickness.ToString();
+        }
+    }
+}
